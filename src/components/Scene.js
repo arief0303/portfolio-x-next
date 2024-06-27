279efc5e90f71bf91016e209ec022b59ca1ae5ce
Arief Raihan Syauqie <arief0303@gmail.com>
2024-06-27 19:50:13 +0700

    chore: Improve loading experience with lazy loading for assets in Scene component

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { MarchingCubes, Environment, Bounds, Float, useProgress, Html } from '@react-three/drei'
 import { Physics } from '@react-three/rapier'
 import MetaBall from './MetaBall'
 import Pointer from './Pointer'
+import { a, useTransition } from "@react-spring/web";
 
 function useProgressMock() {
   const [progress, setProgress] = useState(0); // Initial progress
@@ -35,32 +36,23 @@ function useProgressMock() {
 
 function Loader() {
   const { active, progress } = useProgress();
-
-  if (!active) return null;
-
-  return (
-    <Html>
-      <div style={{ width: '100%', backgroundColor: '#eee', borderRadius: '2px', margin: '20px 0' }}>
-        <div style={{
-          position: 'fixed',
-          zIndex: 100,
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          height: '20px',
-          width: '17vw',
-          background: `linear-gradient(to right, #ffffff ${progress}%, #d9d9d9 ${progress}%)`,
-          borderRadius: '2px',
-          transition: 'background 0.5s ease-in-out, opacity 0.5s ease-in-out',
-          textAlign: 'center',
-          color: 'black',
-          lineHeight: '20px',
-          opacity: progress === 100 ? 0 : 1,
-        }}>
-          {Math.round(progress)}%
-        </div>
-      </div>
-    </Html>
+  const transition = useTransition(active, {
+    from: { opacity: 1, progress: 0 },
+    leave: { opacity: 0 },
+    update: { progress },
+  });
+  return transition(
+    ({ progress, opacity }, active) =>
+      active && (
+        <>
+          <a.div className='loading' style={{ opacity }}>
+            <div className='loading-bar-container'>
+              <a.div className='loading-bar' style={{ width: progress }}></a.div>
+              <div className='text-center text-black'>loading</div>
+            </div>
+          </a.div>
+        </>
+      )
   );
 }
 
@@ -79,35 +71,33 @@ export default function Scene() {
 
   return (
     <>
-      {isLoading ? (
-        <Loader /> // Show loader while loading
-      ) : (
-        <Canvas dpr={[1, 1.5]} camera={{ position: [0, 0, 5], fov: 25 }}>
-          <color attach="background" args={['#f0f0f0']} />
-          <ambientLight intensity={1} />
-          <Physics gravity={[0, 2, 0]}>
-            <Float>
-              <MarchingCubes resolution={40} maxPolyCount={20000} enableUvs={false} enableColors>
-                <meshStandardMaterial vertexColors thickness={0.15} roughness={0} />
-                <MetaBall color="indianred" position={[1, 1, 0.5]} />
-                <MetaBall color="skyblue" position={[-1, -1, -0.5]} />
-                <MetaBall color="teal" position={[2, 2, 0.5]} />
-                <MetaBall color="orange" position={[-2, -2, -0.5]} />
-                <MetaBall color="hotpink" position={[3, 3, 0.5]} />
-                <MetaBall color="aquamarine" position={[-3, -3, -0.5]} />
-                <Pointer />
-              </MarchingCubes>
-            </Float>
-          </Physics>
+      <Loader />
+      <Canvas dpr={[1, 1.5]} camera={{ position: [0, 0, 5], fov: 25 }}>
+        <color attach="background" args={['#f0f0f0']} />
+        <ambientLight intensity={1} />
+        <Physics gravity={[0, 2, 0]}>
+          <Float>
+            <MarchingCubes resolution={40} maxPolyCount={20000} enableUvs={false} enableColors>
+              <meshStandardMaterial vertexColors thickness={0.15} roughness={0} />
+              <MetaBall color="indianred" position={[1, 1, 0.5]} />
+              <MetaBall color="skyblue" position={[-1, -1, -0.5]} />
+              <MetaBall color="teal" position={[2, 2, 0.5]} />
+              <MetaBall color="orange" position={[-2, -2, -0.5]} />
+              <MetaBall color="hotpink" position={[3, 3, 0.5]} />
+              <MetaBall color="aquamarine" position={[-3, -3, -0.5]} />
+              <Pointer />
+            </MarchingCubes>
+          </Float>
+        </Physics>
+        <Suspense fallback={null}>
           <Environment files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/industrial_workshop_foundry_1k.hdr" />
-          {/* Zoom to fit a 1/1/1 box to match the marching cubes */}
-          <Bounds fit clip observe margin={1}>
-            <mesh visible={false}>
-              <boxGeometry />
-            </mesh>
-          </Bounds>
-        </Canvas>
-      )}
+        </Suspense>
+        <Bounds fit clip observe margin={1}>
+          <mesh visible={false}>
+            <boxGeometry />
+          </mesh>
+        </Bounds>
+      </Canvas>
     </>
   )
 }
